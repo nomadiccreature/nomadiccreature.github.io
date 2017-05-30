af1e1eeb56ef7b40748f928bf3921e37cce68e83
nomadiccreature <arindra@Arindras-MacBook-Pro.local>
2017-05-30 15:56:04 +0100

    hidden vids

--- locpin/js/main.js
+++ locpin/js/main.js
@@ -39,36 +39,36 @@
 
 
 
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#locpin_video .play");
+// var vid = document.getElementById("bgvid");
+// var pauseButton = document.querySelector("#locpin_video .play");
 
-if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
-    pauseButton.innerHTML = '<img src=\'./img/play-button.png\'>';
-}
+// if (window.matchMedia('(prefers-reduced-motion)').matches) {
+//     vid.removeAttribute("autoplay");
+//     vid.pause();
+//     pauseButton.innerHTML = '<img src=\'./img/play-button.png\'>';
+// }
 
-function vidFade() {
-  vid.classList.add("stopfade");
-}
+// function vidFade() {
+//   vid.classList.add("stopfade");
+// }
 
-vid.addEventListener('ended', function()
-{
-// only functional if "loop" is removed 
-vid.pause();
-// to capture IE10
-vidFade();
-}); 
+// vid.addEventListener('ended', function()
+// {
+// // only functional if "loop" is removed 
+// vid.pause();
+// // to capture IE10
+// vidFade();
+// }); 
 
 
-pauseButton.addEventListener("click", function() {
-  vid.classList.toggle("stopfade");
-  if (vid.paused) {
-    vid.play();
-    pauseButton.innerHTML = '<img src=\'./img/pause-button.png\'>';
-  } else {
-    vid.pause();
-    pauseButton.innerHTML = '<img src=\'./img/play-button.png\'>';
-  }
-})
+// pauseButton.addEventListener("click", function() {
+//   vid.classList.toggle("stopfade");
+//   if (vid.paused) {
+//     vid.play();
+//     pauseButton.innerHTML = '<img src=\'./img/pause-button.png\'>';
+//   } else {
+//     vid.pause();
+//     pauseButton.innerHTML = '<img src=\'./img/play-button.png\'>';
+//   }
+// })
 
