6de43cf1a12713342c73d21680aec19fefd4bddb
nomadiccreature <arindra@arindras-mbp.home>
2017-07-16 16:06:17 +0100

    building new dashboard

--- locpin/js/main.js
+++ locpin/js/main.js
@@ -31,3 +31,14 @@
 //   }
 // })
 
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
